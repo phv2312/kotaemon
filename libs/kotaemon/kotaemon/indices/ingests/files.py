b5fe2c3c886471d2435d69486ba1d2f1fad3362a
phv2312 <kat87yb@gmail.com>
2024-10-02 21:31:03 +0700

    feat: comfort pre-commit

--- libs/kotaemon/kotaemon/indices/ingests/files.py
+++ libs/kotaemon/kotaemon/indices/ingests/files.py
@@ -8,6 +8,7 @@ from theflow.settings import settings as flowsettings
 
 from kotaemon.base import BaseComponent, Document, Param
 from kotaemon.indices.extractors import BaseDocParser
+from kotaemon.indices.ingests.extensions import extension_manager
 from kotaemon.indices.splitters import BaseSplitter, TokenSplitter
 from kotaemon.loaders import (
     AdobeReader,
@@ -17,7 +18,6 @@ from kotaemon.loaders import (
     OCRReader,
     UnstructuredReader,
 )
-from libs.kotaemon.kotaemon.indices.ingests.extensions import extension_manager
 
 unstructured = UnstructuredReader()
 adobe_reader = AdobeReader()
@@ -29,25 +29,6 @@ azure_reader = AzureAIDocumentIntelligenceLoader(
 adobe_reader.vlm_endpoint = azure_reader.vlm_endpoint = getattr(
     flowsettings, "KH_VLM_ENDPOINT", ""
 )
-
-
-# KH_DEFAULT_FILE_EXTRACTORS: dict[str, BaseReader] = {
-#     ".xlsx": PandasExcelReader(),
-#     ".docx": unstructured,
-#     ".pptx": unstructured,
-#     ".xls": unstructured,
-#     ".doc": unstructured,
-#     ".html": HtmlReader(),
-#     ".mhtml": MhtmlReader(),
-#     ".png": ImageReader(),
-#     ".jpeg": ImageReader(),
-#     ".jpg": ImageReader(),
-#     ".tiff": unstructured,
-#     ".tif": unstructured,
-#     ".pdf": PDFThumbnailReader(),
-#     ".txt": TxtReader(),
-#     ".md": TxtReader(),
-# }
 
 
 class DocumentIngestor(BaseComponent):
