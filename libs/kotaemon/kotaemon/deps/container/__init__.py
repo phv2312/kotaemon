a2d4a3f1a90635b9c5ead606261d76086b08e15e
phv2312 <kat87yb@gmail.com>
2024-11-10 09:08:55 +0000

    feat: add #noqa to prevent removing necessary imports

---
+++ libs/kotaemon/kotaemon/deps/container/__init__.py
@@ -0,0 +1,7 @@
+from .documentstore import DocumentStore
+from .fileschema import FileSchema
+from .indexer import Indexer
+from .retriever import Retriever
+from .vectorstore import VectorStore
+
+__all__ = ["VectorStore", "DocumentStore", "FileSchema", "Indexer", "Retriever"]
