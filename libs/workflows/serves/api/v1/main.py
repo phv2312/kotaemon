37c761a954cbf9050cfbd83f9c8fa63f2de4bb3a
cin-niko <niko@cinnamon.is>
2024-12-16 05:03:17 +0000

    feat: add health check

--- libs/workflows/serves/api/v1/main.py
+++ libs/workflows/serves/api/v1/main.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from .routes import indexing, retrieval
+from .routes import indexing, retrieval, utils
 
 api_router = APIRouter()
 api_router.include_router(indexing.router, prefix="/indexing", tags=["indexing"])
 api_router.include_router(retrieval.router, prefix="/retrieval", tags=["retrieval"])
+api_router.include_router(utils.router)
